66fa9b4d573a01a737007d28891a028a314de0ac
koolwood <koolwood@gmail.com>
2025-09-27 22:35:06 -0400

    added ut sensor file added more variables to eeprom commented out radar light on command added gpio for red greeen yellow lights added more cotrol for the blue tooth interface

--- ld016_radar.ino
+++ ld016_radar.ino
@@ -1,10 +1,17 @@
 #include "ld016.h"
 #include "ble.h"
+#include "ut_sensor.h"
 #include <EEPROM.h>
 
 int debug = 0,btupdate = 0;
 int ledPin = 8;
-int distance = 2,light_time = 1,photo = 100,motion_delta = 100,radar_on = 1;
+int redledPin = 2;
+int yellowledPin = 3;
+int greenledPin = 4;
+
+int distance = 2,light_time = 1,photo = 100,motion_delta = 100,radar_on = 1,ut_distance = 1;
+int yellow_limit = 7,green_limit = 10,red_limit = 3,motion_time_limit = 5 ;
+bool stream_data = false;
 
 
 void setup(){
@@ -17,21 +24,63 @@ void setup(){
   load_up_eeprom(false,false);
 
   blsetup();
-
+  utsetup();
   pinMode(ledPin, OUTPUT);
+  pinMode(redledPin, OUTPUT);
+  pinMode(yellowledPin, OUTPUT);
+  pinMode(greenledPin, OUTPUT);
   pinMode(6, INPUT_PULLUP);
-//  get_ldo16_setup();
   setup_ld016();
   get_ldo16_setup();
- }
+  digitalWrite(greenledPin,1); 
+  digitalWrite(yellowledPin,1); 
+  digitalWrite(redledPin,1); 
+  delay(1000);
+  digitalWrite(greenledPin,0); 
+  digitalWrite(yellowledPin,0); 
+  digitalWrite(redledPin,0); 
+  }
 
-void loop()
-{
-//  int val = Sensor.readPresenceDetection();
-static bool val = false;
+void loop(){
+  static int motion = 0,cycle_started = 0;
+  static int ut_distance;
+  if(digitalRead(6)){
+    motion += 1;
+    if(motion > motion_time_limit)motion = motion_time_limit;
+  }else{
+     motion -= 1;
+     if(motion < 0)motion = 0;
+  }
   digitalWrite(ledPin,digitalRead(6));
-  val = !val;
+  ut_distance = get_ut_distance();
+  if(ut_distance == 0)ut_distance = 100;
+  if((ut_distance > red_limit && motion >= motion_time_limit) || cycle_started ){
+    cycle_started = 1;
+    if(ut_distance > yellow_limit){
+      digitalWrite(greenledPin,1); 
+      digitalWrite(yellowledPin,0); 
+      digitalWrite(redledPin,0); 
+    }
+    else if(ut_distance < yellow_limit && ut_distance > red_limit){
+      digitalWrite(greenledPin,0); 
+      digitalWrite(yellowledPin,1); 
+      digitalWrite(redledPin,0); 
+    }
+    else if(ut_distance <  red_limit){
+      digitalWrite(greenledPin,0); 
+      digitalWrite(yellowledPin,0); 
+      digitalWrite(redledPin,1); 
+    }
+  }
+  if(cycle_started && motion == 0){
+    cycle_started = 0;
+    digitalWrite(greenledPin,0); 
+    digitalWrite(yellowledPin,0); 
+    digitalWrite(redledPin,0); 
+  }
   delay(500);
+  Serial.printf("motion = %d,cycle_started = %d\n\r",motion,cycle_started);
+  if(stream_data) blloop(String("motion = ") + String(motion) + String(", cycle_started =") + String(cycle_started) + String(", ut distance =") + String(ut_distance) +  String("\r\n"));
   process_input();
 }
 
@@ -53,11 +102,11 @@ void setup_ld016(){
   // set_micro_motion_detection_sensing_distance(15);
   // set_light_on_off(0);
   // delay(1000);
-  set_light_on_off(radar_on);
-  delay(1000);
+  // set_light_on_off(radar_on);
+  //delay(1000);
 //  set_PWM_Duty_Cycle(500);//50%
-  set_radar_on_off(0);
-  Serial.printf("get_radar_on_off = %d\n\r",get_radar_on_off());
+  // set_radar_on_off(0);
+  // Serial.printf("get_radar_on_off = %d\n\r",get_radar_on_off());
   set_radar_on_off(1);
   Serial.printf("get_radar_on_off = %d\n\r",get_radar_on_off());
   // set_save_radar(1);
@@ -66,6 +115,7 @@ void setup_ld016(){
   // Serial.printf("get_save_radar_status = %d\n\r",get_save_radar_status());
 }
 void load_up_eeprom(bool clear_data,bool update){
+  String mess;
   int eeprom_initialized = 0;
   EEPROM.get(0,eeprom_initialized);
   if(eeprom_initialized != 1 || clear_data){
@@ -74,6 +124,9 @@ void load_up_eeprom(bool clear_data,bool update){
     photo = 100;
     motion_delta = 100;
     radar_on = 1;
+    yellow_limit = 50;
+    red_limit = 20;
+    motion_time_limit = 5;
   }
   if(eeprom_initialized != 1 || update || clear_data){
     EEPROM.put(0,1);
@@ -82,20 +135,35 @@ void load_up_eeprom(bool clear_data,bool update){
     EEPROM.put(12,photo);
     EEPROM.put(16,motion_delta);
     EEPROM.put(20,radar_on);
+    EEPROM.put(24,yellow_limit);
+    EEPROM.put(28,red_limit);
+    EEPROM.put(32,motion_time_limit);
   }else{
     EEPROM.get(4,distance);
     EEPROM.get(8,light_time);
     EEPROM.get(12,photo);
     EEPROM.get(16,motion_delta);
     EEPROM.get(20,radar_on);
+    EEPROM.get(24,yellow_limit);
+    EEPROM.get(28,red_limit);
+    EEPROM.get(32,motion_time_limit);
   }
   EEPROM.commit();
+  mess = String("radar distance = ") + String(distance) +String("\r\n") + String("radar light = ") + String(light_time) +String("\r\n");
+  blloop(mess);
+  mess = String("radar photo = ") + String(photo) +String("\r\n") + String("radar motion = ") + String(motion_delta) +String("\r\n");
+  blloop(mess);
+  mess = String("yellow limit = ") + String(yellow_limit) +String("\r\n") + String("red limit= ") + String(red_limit) +String("\r\n");
+  blloop(mess);
+  mess = String("time limit = ") + String(motion_time_limit) +String("\r\n ");
+  blloop(mess);
+
 }
 
 void process_input() {
   String mess;
   if (rxdata != 0) {
-    if (rxdata == 'h') mess = "connected\r\nc=clear eeprom, d=distance, l=light time\r\np=photo, m=motion delta, w=update eeprom\r";
+    if (rxdata == 'h')mess = "connected\r\nCapital letter increases value\r\nlower case decreases value\r\n\r\nc=clear eeprom, d=distance, l=light time\r\np=photo, m=motion delta, w=update eeprom\r\ny=yellow limit, r=red limit, t=motion time\r\ns=get setup\r\n";
     else if (rxdata == 'c'){ mess = "clearing data\r\n";load_up_eeprom(true,false);}//reset eeprom
     else if (rxdata == 'D'){distance++;if(distance>15)distance=15; mess = String("increased distance to ") + String(distance) +String("\r\n");}
     else if (rxdata == 'd'){distance--;if(distance<1)distance=1; mess = String("decreased distance to ") + String(distance) +String("\r\n");}
@@ -105,7 +173,15 @@ void process_input() {
     else if (rxdata == 'p'){photo-=100;if(photo<100)photo=100; mess = String("decreased photo to ") + String(photo) +String("\r\n");}
     else if (rxdata == 'M'){motion_delta+=100;if(motion_delta>2000)motion_delta=2000; mess = String("increased motion_delta to ") + String(motion_delta) +String("\r\n");}
     else if (rxdata == 'm'){motion_delta-=100;if(motion_delta<100)motion_delta=100; mess = String("decreased motion_delta to ") + String(motion_delta) +String("\r\n");}
-    else if (rxdata == 'w'){ mess = "update eeprom\r\n";load_up_eeprom(false,true);}
+    else if (rxdata == 'Y'){yellow_limit+=5;if(yellow_limit>80)yellow_limit=80; mess = String("increased yellow_limit to ") + String(yellow_limit) +String("\r\n");}
+    else if (rxdata == 'y'){yellow_limit-=5;if(yellow_limit<red_limit + 5)yellow_limit=red_limit + 5; mess = String("decreased yellow_limit to ") + String(yellow_limit) +String("\r\n");}
+    else if (rxdata == 'R'){red_limit+=5;if(red_limit>yellow_limit - 5)red_limit=yellow_limit - 5; mess = String("increased red_limit to ") + String(red_limit) +String("\r\n");}
+    else if (rxdata == 'r'){red_limit-=5;if(red_limit<5)red_limit=5; mess = String("decreased red_limit to ") + String(red_limit) +String("\r\n");}
+    else if (rxdata == 'T'){motion_time_limit+=2;if(motion_time_limit>60)motion_time_limit=60; mess = String("increased motion_time_limit to ") + String(motion_time_limit) +String("\r\n");}
+    else if (rxdata == 't'){motion_time_limit-=2;if(motion_time_limit<4)motion_time_limit=4; mess = String("decreased motion_time_limit to ") + String(motion_time_limit) +String("\r\n");}
+    else if (rxdata == 'w'){mess = "update eeprom\r\n";load_up_eeprom(false,true);}
+    else if (rxdata == 's')load_up_eeprom(false,false); //send setup
+    else if (rxdata == '1'){stream_data = !stream_data;if(stream_data) mess = String("stream on\r\n");else  mess = String("stream off\r\n");}
     else mess = "invalid data send 'h' for help\n";
     setup_ld016();
     get_ldo16_setup();
